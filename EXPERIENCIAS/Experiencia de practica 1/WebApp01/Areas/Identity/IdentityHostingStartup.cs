cf417fa2bc27c5034a7adf40fa07668328efd1f8
christian Chili <christian.chili9@gmail.com>
2021-11-21 01:25:51 -0500

    EXPERIENCIAS Y EJERCICIO PROPUESTO

---
+++ EXPERIENCIAS/Experiencia de practica 1/WebApp01/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WebApp01.Data;
+
+[assembly: HostingStartup(typeof(WebApp01.Areas.Identity.IdentityHostingStartup))]
+namespace WebApp01.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
